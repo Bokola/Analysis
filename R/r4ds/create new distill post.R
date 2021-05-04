02b7a0cf32c0649d8a397df67fa1a094c0e076f7
Bokola <33178339+Bokola@users.noreply.github.com>
2021-05-05 01:00:15 +0200

    add web scraping folder

--- R/r4ds/create new distill post.R
+++ R/r4ds/create new distill post.R
@@ -25,6 +25,9 @@ data_dir = file.path(
 )
 data = readr::read_csv(file.path(data_dir, "G8.pilot.data.csv"))
 site_dir = file.path(home, "Distill websites", "_posts")
+site_dir2 = file.path(home, "Distill websites")
 # set dir to distill website location
-setwd(site_dir)
-create_post("sample size calculation for GLMM model with a log link")
+setwd(site_dir2)
+library(distill)
+# distill::import_post('https://bbolker.github.io/mixedmodels-misc/glmmFAQ.html')
+create_post("Web scraping with R")
