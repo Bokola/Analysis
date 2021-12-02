1e29f559b008ccb285ad5ac0d5f4610f0148e939
Bokola <33178339+Bokola@users.noreply.github.com>
2021-12-02 21:49:29 +0100

    day2 part 2

--- advent of code 2021/day2.R
+++ advent of code 2021/day2.R
@@ -53,11 +53,64 @@ ipk(list.pkg)
 home <- ifelse(Sys.info()["sysname"] == "Linux", Sys.getenv("HOME"), Sys.getenv("USERPROFILE")) %>%
   gsub("\\\\", "/",.) %>% file.path(., "Analysis", "advent of code 2021")
 
-data <- read.table(file.path(home, "day2_data.txt"), header = F)  %>% `colnames<-`(c("x", "y"))
+data <- read.table(file.path(home, "day2_data.txt"), header = F)  %>% `colnames<-`(c("direction", "distance"))
 
 data <- data %>%
-  mutate(f = ifelse(x == "forward", y, 0),
-         v = ifelse(x == 'up', y*-1,
-                    ifelse(x == 'down', y, 0)))
+  mutate(f = ifelse(direction == "forward", distance, 0),
+         v = ifelse(direction == 'up', distance * -1,
+                    ifelse(direction == 'down', distance, 0)))
 
 data %>% summarize(v = sum(v), f = sum(f)) %>% prod()
+
+# --- Part Two ---
+#   Based on your calculations, the planned course doesn't seem to make any sense.
+#   You find the submarine manual and discover that the process is actually slightly more complicated.
+# 
+# In addition to horizontal position and depth, you'll also need to track a third value,
+# aim, which also starts at 0. The commands also mean something entirely different than you first thought:
+#   
+#   down X increases your aim by X units.
+# up X decreases your aim by X units.
+# forward X does two things:
+#   It increases your horizontal position by X units.
+# It increases your depth by your aim multiplied by X.
+# Again note that since you're on a submarine, down and up do the opposite of
+# what you might expect: "down" means aiming in the positive direction.
+# 
+# Now, the above example does something different:
+# 
+# forward 5 adds 5 to your horizontal position, a total of 5. Because your aim is 0, your depth does not change.
+# down 5 adds 5 to your aim, resulting in a value of 5.
+# forward 8 adds 8 to your horizontal position, a total of 13. Because your aim is 5, your depth increases by 8*5=40.
+# up 3 decreases your aim by 3, resulting in a value of 2.
+# down 8 adds 8 to your aim, resulting in a value of 10.
+# forward 2 adds 2 to your horizontal position, a total of 15. Because your aim is 10,
+# your depth increases by 2*10=20 to a total of 60.
+# After following these new instructions, you would have a horizontal position of 15
+# and a depth of 60. (Multiplying these produces 900.)
+# 
+# Using this new interpretation of the commands, calculate the horizontal position
+# and depth you would have after following the planned course. What do you get if 
+# you multiply your final horizontal position by your final
+
+directions <- tribble(
+  ~ direction, ~x, ~ y,
+  "down", 0, 1,
+  "up", 0, -1,
+  "forward", 1, 0
+)
+
+joined <- data %>% inner_join(directions, by = "direction")
+
+# part 1
+joined %>% 
+  summarize(horizontal = sum(distance * x),
+            depth = sum(distance * y),
+            product = horizontal * depth)
+
+# part 2
+
+joined %>% mutate(aim = cumsum(distance * y)) %>%
+  summarize(horizontal = sum(distance * x),
+            depth = sum(aim * distance * x),
+            product = horizontal * depth)
