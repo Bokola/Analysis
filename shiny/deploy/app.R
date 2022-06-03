90bbb0108fe203133b62a7d150b3cbd9a49d65f9
bokola <okolabasilowiti@gmail.com>
2022-06-03 08:27:19 +0300

    deploy to shinyapps.io via rconnect

---
+++ shiny/deploy/app.R
@@ -0,0 +1,62 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+ipk <- function(pkg) {
+  new_pkg <- list_pkg[!(list_pkg %in% .packages(all.available = T))]
+  if (length(new_pkg))
+    install.packages(new_pkg, dependencies = T,
+                     repos = "https://cran.us.r-project.org",
+                     lib = .libPaths()[[1]])
+  sapply(pkg, require, character.only = T)
+}
+list_pkg <- c(
+  "gapminder", "ggforce", "gh", "globals", "openintro", "profvis",
+  "RSQLite", "shiny", "shinycssloaders", "shinyFeedback",
+  "shinythemes", "testthat", "thematic", "tidyverse", "vroom",
+  "waiter", "xml2", "zeallot"
+)
+ipk(list_pkg)
+
+ui <- fluidPage( # layout function that sets up the basic str of page
+  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"), # for code output
+  tableOutput("table")
+)
+#outputs are added on server with output$id assignments
+server <- function(input, output, session){
+  output$summary <- renderPrint({
+    # print results of an expression
+    dataset <- get(input$dataset, "package:datasets") #get by name
+    summary(dataset)
+  })
+  # we assigned dataset object twice
+  
+  output$table <- renderTable({
+    dataset <- get(input$dataset, "package:datasets")
+    dataset
+  })
+  
+}
+# you create a reactive expression by wrapping block of code in reactive({...})
+server <- function(input, output, session){
+  # create a reactive expression
+  dataset <- reactive({
+    get(input$dataset, "package:datasets")
+  })
+  output$summary <- renderPrint({
+    # use a reactive expression by calling it like a function
+    summary(dataset())
+    output$table <- renderTable({
+      dataset()
+    })
+  })
+}
+shinyApp(ui, server)
+
---
+++ shiny/deploy_with_rconnect.R
@@ -0,0 +1,22 @@
+.libPaths()
+ipk <- function(pkg) {
+  new.pkg <- list.pkg[!(list.pkg %in% .packages(all.available = T))]
+  if (length(new.pkg))
+    install.packages(new.pkg,
+                     repos = "https://cran.us.r-project.org",
+                     dependencies = T,
+                     lib = .libPaths()[[1]])
+  sapply(pkg, require, character.only = T)
+}
+list.pkg <- c("tidyverse", "magrittr", "rsconnect")
+ipk(list.pkg)
+
+# authorize rconnect to access shinyapps.io
+rsconnect::setAccountInfo(name = 'bokola',
+                          token = 'F8DD64A33717C54BA5B5AD1664C89CAC',
+                          secret = 'r5gBSUltMB+oCfIqbd0kWRZ0ORLksjtH3y3HdX0X')
+# deploy
+home.dir <- ifelse(.Platform$OS.type == "windows", Sys.getenv("USERPROFILE"),
+         Sys.getenv("HOME")) %>% gsub("\\\\", "/",.)
+app.dir <- file.path(home.dir, "Analysis", "shiny", "deploy")
+rsconnect::deployApp(app.dir)
